d187f5da326b4b89f28d8c1431089e46b1a8618d
Cloak <JustinAves@null.net>
2023-02-24 16:57:28 -0500

    Add universal to loader

--- main.lua
+++ main.lua
@@ -1,2 +1,4 @@
 if game.PlaceId == 579955134 then 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaSpace/luaspace/main/Games/Auto%20Rap%20Battles.lua",true))()
+else
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaSpace/luaspace/main/Universal/universal.lua",true))()
